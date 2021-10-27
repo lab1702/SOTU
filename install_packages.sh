1c847d1b27b549de0a7f52d46b5b351b9fb6530f
lab1702 <lab1702@gmail.com>
2021-10-27 11:05:09 -0400

    Update install_packages.sh
    udpipe is now in the repos

--- install_packages.sh
+++ install_packages.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-mamba install -y r-tidytext r-topicmodels r-sentimentr
-
-R -e 'install.packages("udpipe", repos="https://cran.r-project.org")'
+mamba install -y r-tidytext r-topicmodels r-sentimentr r-udpipe
